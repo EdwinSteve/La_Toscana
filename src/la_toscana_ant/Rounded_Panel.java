f3af1bc56d772388b3eb4b43b1a2607eccaadd6d
Edwin Steve Suarez Rivera <edwinstevesuarezrivera@gmail.com>
2023-05-21 21:48:13 -0500

    created repository cafeteria la toscana

---
+++ src/la_toscana_ant/Rounded_Panel.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package la_toscana_ant;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class Rounded_Panel extends JPanel {
+
+    /** Stroke size. it is recommended to set it to 1 for better view */
+    protected int strokeSize = 1;
+    /** Color of shadow */
+    protected Color shadowColor = Color.black;
+    /** Sets if it drops shadow */
+    protected boolean shady = true;
+    /** Sets if it has an High Quality view */
+    protected boolean highQuality = true;
+    /** Double values for Horizontal and Vertical radius of corner arcs */
+    protected Dimension arcs = new Dimension(20, 20);
+    /** Distance between shadow border and opaque panel border */
+    protected int shadowGap = 5;
+    /** The offset of shadow.  */
+    protected int shadowOffset = 4;
+    /** The transparency value of shadow. ( 0 - 255) */
+    protected int shadowAlpha = 150;
+
+	//FOLLOWING CODES GOES HERE
+}
